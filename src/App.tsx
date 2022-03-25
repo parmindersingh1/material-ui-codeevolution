60551822088987c7d617b9d3a317c45f3ad20122
Parminder Singh <parminder.s@gmail.com>
2022-03-25 15:15:24 +0530

    accordion changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { MuiCard } from "./components/MuiCard";
+import { MuiAccordion } from "./components/MuiAccordion";
+// import { MuiCard } from "./components/MuiCard";
 // import { MuiLayout } from "./components/MuiLayout";
 // import { MuiAutocomplete } from "./components/MuiAutocomplete";
 // import { MuiRating } from "./components/MuiRating";
@@ -23,8 +24,8 @@ function App() {
     {/* <MuiRating /> */}
     {/* <MuiAutocomplete /> */}
     {/* <MuiLayout /> */}
-
-    <MuiCard />
+    {/* <MuiCard /> */}
+    <MuiAccordion />
   </div>;
 }
 
